69335a0688ef6fcb7ec980ee366f3a444e760cc2
Felix Philips <www.felix4real98@gmail.com>
2022-05-11 14:26:48 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+export * from './middlewares/require-user-role';
 
 export * from './events/base-listener';
 export * from './events/base-publisher';
@@ -20,7 +21,6 @@ export * from './events/subscription/subscription-created-event';
 export * from './events/subscription/subscription-updated-event';
 export * from './events/subscription/subscription-payment-created';
 export * from './events/subjects';
-
 export * from './types/user-roles';
 export * from './types/subscription';
 export * from './data/subscription';
