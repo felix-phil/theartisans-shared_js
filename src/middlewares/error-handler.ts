58ff9bd1ecba1fea9c88bb08b440f871e074e135
felix-phil <www.felix4real98@gmail.com>
2024-03-12 06:01:05 +0100

    feature: error-handler middleware

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../errors/custom-error';
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -16,7 +16,7 @@ export const errorHandler = (
   res.status(400).send({
     errors: [
       {
-        message: 'Something went wrong',
+        message: "Something went wrong",
       },
     ],
   });
